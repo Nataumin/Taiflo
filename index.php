7be7519f1e518ece79a1afd403e9edf18d46166b
Nataumin <89603207+Nataumin@users.noreply.github.com>
2021-09-03 19:04:29 +0100

    Update index.php

--- index.php
+++ index.php
@@ -11,5 +11,20 @@
   include("compiler.php");
 ?>
 
+<script>
+    $(document).ready(function() {
+
+        let input = document.getElementById('input');
+        let output = document.getElementById('output');
+        
+        $("#input").load("stream/1/01/compiled.md", function()
+            {
+                output.innerHTML = convert(input.innerText);
+            });
+    });
+</script>
+
+<div id="input" style="display: none;"></div>
+<div id="output"></div>
 </body>
 </html>
